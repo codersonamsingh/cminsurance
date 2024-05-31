541741033a96b6e0f7f0f72a477e32919a41857d
codersonamsingh <sonam.sin.rajput@gmail.com>
2024-05-31 20:34:41 +0530

    Navbar.css running

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,13 +9,26 @@ const Navbar = () => {
             <img className ='logo' src={logo} alt="" />
         </div>
 
-        {/* <div className="nav-right flex-div">
-           <h3 className="menu-item">mn1  </h3>
-           <h3 className="menu-item">mn2  </h3>
-           <h3 className="menu-item last-menu"> <FaUser /> mn3</h3>
-      <button className="create-button">Create Account</button>
+        {<div className="nav-middle flex-div">
+           <h3 className="menu-item">General Insurance </h3>
+           <h3 className="menu-item">Life Insurance</h3>
+           <h3 className="menu-item">Insurance Article</h3>
+           
+           </div>}
 
-        </div> */}
+
+        { <div className="nav-right flex-div">
+           <h3 className="menu-item">About Us </h3>
+           <h3 className="menu-item">Contact us </h3>
+           <h3 className="menu-item">Become a Sales Partner </h3>
+           <h3 className="menu-item">Claim </h3>
+           <h3 className="menu-item">SMC Assure </h3>
+           {/* <h3 className="menu-item last-menu"> <FaUser /> mn3</h3> */}
+      <button className="create-button">Login</button>
+
+        </div> }
+
+
     </nav>
     )
 }
